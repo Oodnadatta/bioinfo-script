13ba9baef480a03e0b1636885a3e621de6548839
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2018-11-11 17:42:49 +0100

    Parse with csv.reader

--- postpipelinedijon.py
+++ postpipelinedijon.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python3
 
+import csv
 import sys
 
 if len(sys.argv) != 2:
 	print('Usage: postpipelinedijon.py filename', file=sys.stderr)
 	sys.exit(1)
-with open(sys.argv[1], 'r') as file:
-	for line in file:
-		print(line)
-	
+with open(sys.argv[1], 'r') as tsvfile:
+	tsvreader = csv.reader(tsvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
+	#TODO : gérer les fichiers vides
+	header = next(tsvreader)
+	print(header)
+	dictionnary = {columnname:columnnumber for columnnumber, columnname in enumerate(header)}
+	print (dictionnary)
+	#for row in tsvreader:
+	#	print(row)
+		
