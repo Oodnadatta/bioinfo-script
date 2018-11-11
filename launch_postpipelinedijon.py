ca4461e98cb6e5d6b219fef196fdbda153141551
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2018-11-11 23:10:05 +0100

    Create launch_postpipelinedijon.py

---
+++ launch_postpipelinedijon.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import sys
+import postpipelinedijon
+
+# python "C:\Users\asden\Desktop\a jeter\bioinfo-script\postpipelinedijon.py" "C:\Users\asden\Desktop\Exomes Dijon\*.report.tsv" "C:\Users\asden\Desktop\Exomes Dijon\*.report.filtered.tsv"
