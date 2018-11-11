f98b410502b9980275e98a7793fe556cf8fd4a30
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2018-11-11 18:57:39 +0100

    Print a tsv file in output

--- postpipelinedijon.py
+++ postpipelinedijon.py
@@ -3,19 +3,23 @@
 import csv
 import sys
 
-if len(sys.argv) != 2:
-	print('Usage: postpipelinedijon.py filename', file=sys.stderr)
+if len(sys.argv) != 3:
+	print('Usage: postpipelinedijon.py inputfilename.tsv outputfilename.tsv', file=sys.stderr)
 	sys.exit(1)
-with open(sys.argv[1], 'r') as tsvfile:
-	tsvreader = csv.reader(tsvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
+with open(sys.argv[1], 'r', newline='') as inputtsvfile:
+	tsvreader = csv.reader(inputtsvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
 	#TODO: support empty files
 	header = next(tsvreader)
-	print(header)
 	#TODO: reprint header to be in tsv and not in csv
 	dictionnary = {columnname:columnnumber for columnnumber, columnname in enumerate(header)}
 	rows = []
 	for row in tsvreader:
 		rows.append(row)
 
+with open(sys.argv[2], 'w', newline='') as outputtsvfile:
+	tsvwriter = csv.writer(outputtsvfile, delimiter='\t')
+	tsvwriter.writerow(header)
+	for row in rows:
+		tsvwriter.writerow(row)
 
 		
