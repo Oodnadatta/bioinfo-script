c619ce698486e5dbc2a53ea08329a9e6187d0bd2
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2018-11-11 19:51:24 +0100

    Make filtering generic

--- postpipelinedijon.py
+++ postpipelinedijon.py
@@ -19,12 +19,15 @@ with open(sys.argv[1], 'r', newline='') as inputtsvfile:
 with open(sys.argv[2], 'w', newline='') as outputtsvfile:
 	tsvwriter = csv.writer(outputtsvfile, delimiter='\t')
 	tsvwriter.writerow(header)
-	todelete = []
-	for rownumber, row in enumerate(rows):
-		if 'pathogenic' in row[dictionnary['ClinVar']].lower() and 'conflicting_interpretations_of_pathogenicity' not in row[dictionnary['ClinVar']].lower():
-			tsvwriter.writerow(row)
-			todelete.append(rownumber)
-	for rownumber in todelete:
-		del rows[rownumber]
-
-		
+	def keep_if(condition):
+		todelete = []
+		for rownumber, row in enumerate(rows):
+			if condition(row):
+				tsvwriter.writerow(row)
+				todelete.append(rownumber)
+		for rownumber in todelete:
+			del rows[rownumber]
+	def is_pathogenic_in_clinvar(row):
+		return 'pathogenic' in row[dictionnary['ClinVar']].lower() and 'conflicting_interpretations_of_pathogenicity' not in row[dictionnary['ClinVar']].lower()
+	keep_if(is_pathogenic_in_clinvar)
+	
