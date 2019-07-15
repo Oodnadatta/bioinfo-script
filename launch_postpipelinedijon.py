5fcde69eb21621395998d7e639a2f036d2632025
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2019-07-15 20:36:09 +0200

    Manage empty files

--- launch_postpipelinedijon.py
+++ launch_postpipelinedijon.py
@@ -5,5 +5,13 @@ import sys
 
 import postpipelinedijon
 
+empty_file_exception = False
+
 for inputtsvfilename in glob.glob(r'C:\Users\asden\Desktop\Exomes Dijon\*.report.tsv'):
-	postpipelinedijon.filter_variants(inputtsvfilename, inputtsvfilename.replace('report.tsv', 'report.filtered.tsv'))
+	try:
+		postpipelinedijon.filter_variants(inputtsvfilename, inputtsvfilename.replace('report.tsv', 'report.filtered.tsv'))
+	except postpipelinedijon.EmptyFileException as ex:
+		empty_file_exception = True
+
+if empty_file_exception:
+	sys.exit(1)
